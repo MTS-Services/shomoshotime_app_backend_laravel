73e597ac7ad489c830ff2cb0820d9726ffaf497b
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-01 11:11:26 +0600

    Implement user and content retrieval with admin access control

--- app/Http/Controllers/API/V1/ContentController.php
+++ app/Http/Controllers/API/V1/ContentController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\V1\ContentCollection;
+use App\Models\Content;
+use App\Services\ContentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class ContentController extends Controller
 {
-    //
+     protected ContentService $service;
+
+    public function __construct(ContentService $service)
+    {
+        $this->service = $service;
+    }
+
+       public function getContents(Request $request)
+    {
+        try {
+            $user = request()->user();
+            if (! $user) {
+                return sendResponse(false, 'Unauthorized', null, Response::HTTP_UNAUTHORIZED);
+            }
+
+            if (! $user->isAdmin()) {
+                return sendResponse(false, 'Admin access required', null, Response::HTTP_UNAUTHORIZED);
+            }
+            $type = $request->input('type');
+            $query = $this->service->getContents($type);
+            $contents = $query->paginate($request->input('per_page', 10));
+            $contents->setPageName('page');
+
+            return sendResponse(true, 'Users data fetched successfully.', new ContentCollection($contents), Response::HTTP_OK);
+        } catch (Throwable $e) {
+            Log::error('Get Todos Error: '.$e->getMessage());
+
+            return sendResponse(false, 'Something went wrong.'.$e->getMessage(), null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- routes/api/v1/admin.php
+++ routes/api/v1/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\ContentController;
 use App\Http\Controllers\API\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->prefix('user')->group(function () {
     Route::post('/all', 'getUsers')->name('all-users');
+    Route::post('/single/{id}', 'getUser')->name('single-user');
     Route::post('/status-change', 'statusChange')->name('status-change');
     Route::post('/create', 'store')->name('create-user');
     Route::put('/update/{id}', 'update')->name('user.update');
     Route::delete('/delete/{id}', 'delete')->name('user.delete');
 });
+Route::controller(ContentController::class)->prefix('content')->group(function () {
+    Route::post('/list', 'getContents')->name('list-content');
+    Route::post('/single/{id}', 'getUser')->name('single-user');
+    Route::post('/status-change', 'statusChange')->name('status-change');
+    Route::post('/create', 'store')->name('create-user');
+    Route::put('/update/{id}', 'update')->name('user.update');
+    Route::delete('/delete/{id}', 'delete')->name('user.delete');
+});
+
+
 
