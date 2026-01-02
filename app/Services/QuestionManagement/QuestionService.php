4706fc46d26ade08ebb109e1e9482e0acb3282e7
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-02 16:03:01 +0600

    Enhance QuestionController and QuestionService to support file uploads during question creation and update

--- app/Services/QuestionManagement/QuestionService.php
+++ app/Services/QuestionManagement/QuestionService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\QuestionManagement;
 
+use App\Http\Traits\FileManagementTrait;
 use App\Models\Question;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 
 class QuestionService
 {
+    use FileManagementTrait;
     public function getQuestions(?int $questionId = null, string $orderBy = 'created_at', string $order = 'desc'): Builder
     {
         return Question::with('questionSet')->where('question_set_id', $questionId)->orderBy($orderBy, $order)->latest();
@@ -25,19 +27,38 @@ class QuestionService
         return $model;
     }
 
-    public function createQuestion(array $data)
+    public function createQuestion(array $data , $file = null)
     {
-        return DB::transaction(function () use ($data) {
-
+        return DB::transaction(function () use ($data , $file) {
+             if ($file) {
+                $data['file'] = $this->handleFileUpload(
+                    $file,
+                    'questions',
+                    $data['title'] ?? 'question'
+                );
+            }
             $data['created_by'] = Auth::id();
 
             return Question::create($data);
         });
     }
 
-    public function updateQuestion( $findData, array $data)
+    public function updateQuestion( $findData, array $data , $file = null)
     {
-        return DB::transaction(function () use ($findData, $data) {
+        return DB::transaction(function () use ($findData, $data, $file) {
+
+              if ($file) {
+                if ($findData->file) {
+                    $this->fileDelete($findData->file);
+                }
+
+                // Upload new file
+                $data['file'] = $this->handleFileUpload(
+                    $file,
+                    'questions',
+                    $data['title'] ?? 'question'
+                );
+            }
             $data['updated_by'] = Auth::id();
 
             $findData->update($data);
@@ -49,6 +70,9 @@ class QuestionService
     public function deleteQuestion($findData)
     {
         return DB::transaction(function () use ($findData) {
+            if ($findData->file) {
+                $this->fileDelete($findData->file);
+            }
             $findData->forceDelete();
         });
     }
