810b60c16c277aa95005be37ec3b3c430aa5fb9f
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-02 14:41:46 +0600

    add Question realated controller  request Resource

---
+++ app/Http/Resources/API/V1/QuestionResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\API\V1;
+
+use App\Models\QuestionSet;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category' => $this->category ?? 'N/A',
+            'title' => $this->title ?? 'N/A',
+            'subtitle' => $this->subtitle ?? 'N/A',
+            'status' => $this->status,
+            'status_label' => $this->status_label,
+            'status_data' => QuestionSet::getStatusList(),
+            'status_color' => $this->status_color,
+            'created_at' => $this->created_at_formatted,
+            'updated_at' => $this->updated_at_formatted,
+            'creater_name' => $this->creater?->name ?? 'N/A',
+            'updater_name' => $this->updater?->name ?? 'N/A',
+
+        ];
+    }
+}
