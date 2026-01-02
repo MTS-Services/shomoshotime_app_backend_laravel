13eebf0b73c312843d79bfc2c36e5c4ddf3a0583
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-02 14:41:46 +0600

    add Question realated controller  request Resource

---
+++ app/Http/Controllers/API/V1/QuestionManagement/QuestionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\QuestionManagement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    //
+}
