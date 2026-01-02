ffdc8c3edcffa0f69ed67561c689707ff5f066bd
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-02 09:47:27 +0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ContentSeeder::class,
-            FlashCardSeeder::class
+            FlashCardSeeder::class,
+            QuestionSetSeeder::class
         ]);
     }
 }
