e11528ad0e45d3f2f92dbe5159e40e2139be91a0
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-06 17:12:11 +0600

    Refactor QuestionService and add QuestionController: implement getQuestionSets method for fetching question sets with pagination and error handling.

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -1,15 +1,15 @@
 <?php
 
+
 use App\Http\Controllers\API\V1\UserPanel\ContentController;
+use App\Http\Controllers\API\V1\UserPanel\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ContentController::class)->prefix('content')->group(function () {
     Route::post('/study-guides', 'studyGuides')->name('study-guides');
     Route::post('/flash-cards', 'flashCards')->name('flash-cards');
     Route::post('/flash-cards/sets', 'flashCardSets')->name('flash-cards.sets');
-    // Route::post('/single/{id}', 'getUser')->name('single-user');
-    // Route::post('/status-change', 'statusChange')->name('status-change');
-    // Route::post('/create', 'store')->name('create-user');
-    // Route::put('/update/{id}', 'update')->name('user.update');
-    // Route::delete('/delete/{id}', 'delete')->name('user.delete');
+});
+Route::controller(QuestionController::class)->prefix('question')->group(function () {
+    Route::post('/sets', 'getQuestionSets')->name('question-sets');
 });
