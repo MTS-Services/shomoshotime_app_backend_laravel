0dd2a9ff722e69dd9b870acf28dcd2fe74bc1b93
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-06 13:00:16 +0600

    Add search functionality to flashCards method and implement flashCardSets endpoint

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(ContentController::class)->prefix('content')->group(function () {
     Route::post('/study-guides', 'studyGuides')->name('study-guides');
     Route::post('/flash-cards', 'flashCards')->name('flash-cards');
+    Route::post('/flash-cards/sets', 'flashCardSets')->name('flash-cards.sets');
     // Route::post('/single/{id}', 'getUser')->name('single-user');
     // Route::post('/status-change', 'statusChange')->name('status-change');
     // Route::post('/create', 'store')->name('create-user');
