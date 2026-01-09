fea0cb6d6c3ea8deb53c71517b991315ebba07ab
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-09 17:20:37 +0600

    Study guide and flash cards Analaysists model migration creatre done

---
+++ app/Models/StudyGuideActivity.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+class StudyGuideActivity extends BaseModel
+{ 
+    protected $fillable = [
+        'sort_order',
+        'user_id',
+        'content_id',
+        'total_page_attempts',
+
+        'created_by',
+        'updated_by',
+    ];
+
+    
+    
+    /* ===================== ===================== ===================== =====================
+                                    Start of Relation's
+    ===================== ===================== ===================== ===================== */
+   public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function content(): BelongsTo
+    {
+        return $this->belongsTo(Content::class, 'content_id', 'id');
+    }
+
+    /* ===================== ===================== ===================== =====================
+                                    End of Relation's
+    ===================== ===================== ===================== ===================== */
+}
