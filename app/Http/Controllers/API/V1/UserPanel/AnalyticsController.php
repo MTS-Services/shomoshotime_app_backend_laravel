59b1796976dd0e44bedc888e0963ac48896f383d
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-11 17:28:49 +0600

    add controller and service

--- app/Http/Controllers/API/V1/UserPanel/AnalyticsController.php
+++ app/Http/Controllers/API/V1/UserPanel/AnalyticsController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\API\V1\UserPanel;
 
 use App\Http\Controllers\Controller;
+use App\Services\AnalyticsService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class AnalyticsController extends Controller
 {
-    //
+    protected AnalyticsService $service;
+
+    public function __construct(AnalyticsService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function showAnalytics(Request $request)
+    {
+        try {
+            $user = request()->user();
+            if (! $user) {
+                return sendResponse(false, 'Unauthorized', null, Response::HTTP_UNAUTHORIZED);
+            }
+
+
+            return sendResponse(true, 'Analytics data fetched successfully.', null, Response::HTTP_OK);
+        } catch (Throwable $e) {
+            Log::error('Get Todos Error: '.$e->getMessage());
+
+            return sendResponse(false, 'Something went wrong.'.$e->getMessage(), null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- app/Services/AnalyticsService.php
+++ app/Services/AnalyticsService.php
@@ -2,13 +2,18 @@
 
 namespace App\Services;
 
+use App\Models\StudyGuideActivity;
+
 class AnalyticsService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function getAnalyticsData($userId)
     {
-        //
+        $all = StudyGuideActivity::with(['content' => (function ($q) {
+                $q->isPublish()->studyGuide()->select('id','total_pages');
+            })])
+            ->whereHas('content', function ($q) {
+                $q->isPublish()->studyGuide();
+            })
+            ->where('user_id', $userId)->get();
     }
 }
