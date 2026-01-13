06fd08ff9d714043396717e62af6eff5ef330278
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-13 17:41:34 +0600

    User analaycs done

--- app/Http/Controllers/API/V1/UserPanel/AnalyticsController.php
+++ app/Http/Controllers/API/V1/UserPanel/AnalyticsController.php
@@ -18,16 +18,47 @@ class AnalyticsController extends Controller
         $this->service = $service;
     }
 
-    public function showAnalytics(Request $request)
+    public function analytics(Request $request)
     {
         try {
             $user = request()->user();
             if (! $user) {
                 return sendResponse(false, 'Unauthorized', null, Response::HTTP_UNAUTHORIZED);
             }
+            $data = [];
+            $study = $request->input('studyAnalytics', null);
+            $flashcard = $request->input('flashcardAnalytics', null);
+            $practiceAccuracy = $request->input('practiceAccuracy', null);
+            $mocktestAccuracy = $request->input('mockTestAccuracy', null);
+            $practiceProgress = $request->input('practiceProgress', null);
+            $mocktestProgress = $request->input('mockTestProgress', null);
 
+            if($study){
+                $data['studyAnalytics'] = $this->service->overallUserStudyGuideProgress($user->id);
+            } 
+            if($flashcard){
+                $data['flashcardAnalytics'] = $this->service->overallUserFlashcardProgress($user->id);
+            } 
 
-            return sendResponse(true, 'Analytics data fetched successfully.', null, Response::HTTP_OK);
+            if($practiceAccuracy){
+                $data['practiceAccuracy'] = $this->service->getUserOverallPracticeAccuracy($user->id);
+            }
+
+            if($mocktestAccuracy){
+                $data['mocktestAccuracy'] = $this->service->getUserOverallMockAccuracy($user->id);
+            } 
+            if($practiceProgress){
+                $data['practiceProgress'] = $this->service->getUserOverallPracticeProgress($user->id);
+            }
+            if($mocktestProgress){
+                $data['mocktestProgress'] = $this->service->getUserOverallMockProgress($user->id);
+            }
+
+           if(empty($data)){
+                return sendResponse(false, 'No analytics type selected', null, Response::HTTP_BAD_REQUEST);
+           }
+
+            return sendResponse(true, 'User analytics fetched successfully', $data, Response::HTTP_OK);
         } catch (Throwable $e) {
             Log::error('Get Todos Error: '.$e->getMessage());
 
