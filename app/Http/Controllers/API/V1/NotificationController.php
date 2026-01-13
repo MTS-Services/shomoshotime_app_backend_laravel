677d87a0b3172a7e0ccbc35e4b428b6317758f2b
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-13 10:57:54 +0600

    Validation Add

--- app/Http/Controllers/API/V1/NotificationController.php
+++ app/Http/Controllers/API/V1/NotificationController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Events\UserNotificationEvent;
 use App\Events\GlobalNotificationEvent;
-use App\Jobs\SendGlobalNotificationJob;
+use App\Events\UserNotificationEvent;
 use App\Http\Controllers\Controller;
+use App\Jobs\SendGlobalNotificationJob;
 use App\Services\NotificationService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 
 class NotificationController extends Controller
 {
@@ -19,27 +20,30 @@ class NotificationController extends Controller
     {
         $this->service = $service;
     }
-    
+
     public function sendNotification(Request $request): JsonResponse
     {
-        // 1. Validation: Make user_id 'nullable'
-        $data = $request->validate([
+        $validator = Validator::make($request->all(), [
             'user_id' => 'nullable|integer|exists:users,id',
             'title' => 'required|string',
             'message' => 'required|string',
-        ]);        
-        
+        ]);
+
+        if ($validator->fails()) {
+            return sendResponse(false, $validator->errors()->first(), null, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $data = $validator->validated();
         $title = $data['title'];
         $message = $data['message'];
 
-        
-        if (!empty($data['user_id'])) {
+        if (! empty($data['user_id'])) {
             $userId = $data['user_id'];
-            
+
             $this->service->storeNotifications($userId, $data);
-            
+
             event(new UserNotificationEvent($userId, $title, $message));
-            
+
         } else {
             // 1. Fire Global Broadcast immediately (Fastest UI feedback)
             event(new GlobalNotificationEvent($title, $message));
