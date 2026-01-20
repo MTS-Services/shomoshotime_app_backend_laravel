830269aa8a91cb735bd1e1394988aacf432219d1
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-20 09:57:27 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Broadcast;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,36 +18,51 @@ return Application::configure(basePath: dirname(__DIR__))
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
         then: function () {
+            // 1. Admin Routes
             Route::middleware(['auth:api', 'email-verified', 'admin', 'admin-cors'])
                 ->prefix('api/v1/admin')
                 ->name('api.v1.admin.')
                 ->group(base_path('routes/api/v1/admin.php'));
+
+            // 2. User Routes
             Route::middleware(['auth:api', 'email-verified', 'user'])
                 ->prefix('api/v1/user')
                 ->name('api.v1.user.')
                 ->group(base_path('routes/api/v1/user.php'));
 
+            // 3. Public Routes
             Route::prefix('api/v1')
                 ->name('api.v1.')
                 ->group(base_path('routes/api/v1/public.php'));
+            // Broadcast Routes
+            Route::middleware(['auth:api']) 
+                ->prefix('api/v1')
+                ->group(function () {
+                    Broadcast::routes();
+                });
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             HandleCors::class,
         ]);
+
         $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO);
+
         $middleware->alias([
             'auth' => Authenticate::class,
             'email-verified' => App\Http\Middleware\EmailVerified::class,
             'admin' => App\Http\Middleware\AdminMidelware::class,
             'user' => App\Http\Middleware\UserMidelware::class,
             'admin-cors' => App\Http\Middleware\AdminCorsMiddleware::class,
-
         ]);
+
+        // Private Channel
         $middleware->validateCsrfTokens(except: [
+            'api/v1/broadcasting/auth',
             'broadcasting/auth',
         ]);
+
         $middleware->web(MultiLangSet::class);
         $middleware->api(MultiLangSet::class);
     })
