545e5886e4b6190b2370b423fe7dbd600299aa6e
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-01-21 11:22:16 +0600

    Add payment_intent_data to Payment model and migration; add is_premium to User model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -29,6 +29,7 @@ return new class extends Migration {
 
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('last_login_at')->nullable();
+            $table->boolean('is_premium')->default(false);
 
             $table->boolean('is_admin')->default(User::NOT_ADMIN);
 
