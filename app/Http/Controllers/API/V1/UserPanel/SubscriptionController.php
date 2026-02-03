a21b26598b958c66d3c2d49a9cd93f410bd417f2
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2026-02-03 11:48:24 +0600

    update

--- app/Http/Controllers/API/V1/UserPanel/SubscriptionController.php
+++ app/Http/Controllers/API/V1/UserPanel/SubscriptionController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\API\V1\UserPanel;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Services\SubscriptionService;
 use App\Http\Resources\API\V1\SubscriptionResource;
+use App\Services\SubscriptionService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class SubscriptionController extends Controller
 {
-       protected SubscriptionService $service;
+    protected SubscriptionService $service;
+
+    protected static array $staticSecrets = [];
 
     public function __construct(SubscriptionService $service)
     {
@@ -37,5 +39,23 @@ class SubscriptionController extends Controller
         }
     }
 
-    
+    public function stripeKey(Request $request)
+    {
+        try {
+            $user = $request->user();
+            if (! $user) {
+                return sendResponse(false, 'Unauthorized', null, Response::HTTP_UNAUTHORIZED);
+            }
+            $publishableKey = config('services.stripe.key');
+            $secretKey = config('services.stripe.secret');
+
+            return sendResponse(true, 'Key fetched successfully.', [
+                'publishable_key' => $publishableKey,
+                'secret_key' => $secretKey,
+            ], Response::HTTP_OK);
+
+        } catch (Throwable $e) {
+            return sendResponse(false, 'Something went wrong. '.$e->getMessage(), null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
